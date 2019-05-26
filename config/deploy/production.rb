c72fa106fe56537cc85eb2c2241f53488035bf40
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-05-26 20:53:35 +0900

    Update: Basic認証の編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '52.198.151.205', user: 'ec2-user', roles: %w{app db web}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # role-based syntax
 # ==================
 
@@ -60,4 +62,6 @@
 #     # password: "please use keys"
 #   }
 
-server '52.198.151.205', user: 'ec2-user', roles: %w{app db web}
+
+
+
