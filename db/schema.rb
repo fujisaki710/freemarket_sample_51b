c64590b4d2ba6470457aaaa97763a29b12a1de37
batablo <mutemath.us22@gmail.com>
2019-06-03 20:59:25 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_041828) do
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "status", null: false
     t.bigint "item", null: false
+    t.bigint "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
