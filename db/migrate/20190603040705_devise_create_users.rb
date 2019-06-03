ad873fa64c1401975eddbceb6be966684c020cf3
batablo <mutemath.us22@gmail.com>
2019-06-03 13:08:31 +0900

    fix: userモデルの再作成
    rails g devise:install　のコマンドを忘れていた為、再度作成

--- db/migrate/20190603040705_devise_create_users.rb
+++ db/migrate/20190603040705_devise_create_users.rb
@@ -3,15 +3,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :nickname,           null: false
-      t.string :email,              null: false, unique: true
-      t.string :family_name,        null: false
-      t.string :first_name,         null: false
-      t.stirng :family_name_kana,   null: false
-      t.string :first_name_kana,    null: false
-      t.date :birthday,             null: false
-      t.integer :phone_number,      null: false, unique: true
-
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -47,7 +38,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    has_one :profile 
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
