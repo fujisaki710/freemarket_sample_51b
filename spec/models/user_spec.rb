a8fe678792b04fc79ad960e4a790b387d376ac9a
batablo <mutemath.us22@gmail.com>
2019-06-04 10:09:15 +0900

    siderの指摘箇所を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,41 +6,45 @@ end
 
 describe User do
   describe '#create' do
-    it "is valid with an email, password, password_confirmation" do
+    it "email、passwordとpassword_confirmationが存在すれば登録できること" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it "is invalid without an email" do
+    it "emailが空では登録できないこと" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    it "is invalid without a password" do
+    it "passwordが空では登録できないこと" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
 
-    it "is invalid without a password_confirmation although with a password" do
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
-    it "is invalid with a duplicate email address" do
+    it "重複したemailが存在する場合登録できないこと" do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("has already been taken")
     end
 
-    it "is invalid with a password that has less than 5 characters " do
+    it "passwordが6文字以上であれば登録できること" do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      expect(user).to be_valid
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
       user = build(:user, password: "00000", password_confirmation: "00000")
       user.valid?
       expect(user.errors[:password][0]).to include("is too short")
     end
-
   end
 end
