4cccee40b095abf418d3a6e731b5c03fc0ee2b22
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2019-06-05 20:32:37 +0900

    edit 新規登録
    新規登録画面

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,13 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def index2
+  end
+
+  def phone
+  end
+
+  def address
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+  get 'items' => 'items#index2'
+  get 'phone' => 'items#phone'
+  get 'address' => 'items#address'
 end
