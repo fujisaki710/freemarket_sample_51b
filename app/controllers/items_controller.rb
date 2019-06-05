5323e1cab5cc42bb1b975ea2bf2f46142c80b402
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-06-05 10:21:50 +0900

    add: 商品一覧表示、imagesテーブルのカラムの変更
    imagesテーブルのitemカラムをitem_idカラムに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all.order("id DESC").limit(4)
+    @image = Image.all.order("id DESC").limit(4)
+    # @items = Item.includes(:image).page(params[:page]).per(4).order("created_at DESC")
   end
 end
