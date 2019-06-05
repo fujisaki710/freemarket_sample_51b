4102cc36786a715747123093bfe5ed328aa1a4d6
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-06-05 11:18:57 +0900

    edit: コメントアウトの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,5 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order("id DESC").limit(4)
     @image = Image.all.order("id DESC").limit(4)
-    # @items = Item.includes(:image).page(params[:page]).per(4).order("created_at DESC")
   end
 end
