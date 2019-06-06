c4b4426221833ccb3169992cb59c04f799c7a858
fujisaki710 <fujisaki_ykk.710@ezweb.ne.jp>
2019-06-06 15:33:47 +0900

    add: test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,34 @@ class ItemsController < ApplicationController
     @items = Item.all.order("id DESC").limit(4)
     @image = Image.all.order("id DESC").limit(4)
   end
+  
+  def new
+    @item = Item.new
+    # @item.user << current_user
+    # binding.pry
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # Item.create(item_params)
+    # binding.pry
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @image = @item.images
+    binding.pry
+    # @image = Image.find(params[:id])
+
+    @good = Like.where(status:"良い")
+    @normal = Like.where(status:"普通")
+    @bad = Like.where(status:"悪い")
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :discription, :item_condition, :delivery_fee, :shipping_rule, :shipping_area, :shipping_date, :price).merge(user_id: current_user.id)
+  end
+  
 end
