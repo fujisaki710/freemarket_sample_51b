7e24f89d666810337ad23e9ea3a40dc015db6a7e
batablo <mutemath.us22@gmail.com>
2019-06-10 12:24:52 +0900

    add: マークアップ(カテゴリのプルダウン)

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -1,9 +1,30 @@
 $(function() {
-  $(".left-btn--category").on("mouseout", function() {
-    $(".accordion-wrapper").();
+	$(".accordion-wrapper").hide();
+
+  $(".left-btn--category").on("mouseleave", function() {
+		$(".accordion-wrapper").css('display', 'none');
+
 	});
 	
 	$(".left-btn--category").on("mouseenter", function() {
-    $(".accordion-wrapper").css('display', 'block');
-  });
+		$(".accordion-wrapper").css('display', 'block');
+		$(".child-wrapper").hide();
+
+		$(".accordion-parent__category").on("mouseenter", function() {
+			$(".accordion-wrapper").find(".child-wrapper").css('display', 'block');
+			$(".grandchild-wrapper").hide();
+
+			$(".accordion-child__category").on("mouseenter", function() {
+				$(".grandchild-wrapper").css('display', 'block');
+			});
+		});
+	});
+	
+	$(".accordion-wrapper").on("mouseleave", function() {
+		$(this).css('display', 'none');
+	});
+
+	$(".accordion-wrapper").on("mouseenter", function() {
+    $(this).css('display', 'block');
+	});
 });
