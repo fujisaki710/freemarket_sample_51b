c8134493c2768926bf28e0e08ae7d950c91d2779
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-06-10 16:16:04 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order("id DESC").limit(4)
-    @image = Image.all.order("id DESC").limit(4)
+    # @image = Image.all.order("id DESC").limit(4)
   end
   
   def new
