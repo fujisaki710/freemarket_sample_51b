0808478835ba1625ab5a336b208bf0fee01d7be8
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2019-06-11 11:52:02 +0900

    add-商品削除機能
    ・削除ボタンと編集ボタンを作成。
    ・商品購入ページのファイル名を一時的にshow3に変更。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("id DESC").limit(4)
@@ -16,6 +16,13 @@ class ItemsController < ApplicationController
     redirect_to item_path(@item)
   end
 
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    end
+  end
+
   def show
     @images = @item.images
     @goods = Like.where(status: "良い")
@@ -24,6 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  has_many_attached :images
+  has_many_attached :images, dependent: :destroy
   has_one :like
-  has_many :comments, dependent: :destroy
   has_many :items_categories
   has_many :categories, through: :items_categories
   belongs_to :brand,optional: true
   belongs_to :user
+  # 現状はコメント機能の実装を行わないため、コメントアウトしております
+  # has_many :comments, dependent: :destroy
 end
