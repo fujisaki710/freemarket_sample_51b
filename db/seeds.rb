31c2da6f88e8613eff6ee1077445ce4045e154ce
batablo <mutemath.us22@gmail.com>
2019-06-14 11:09:15 +0900

    初期データ画像の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,50 +24,65 @@ mens_pants.children.create([{name: "スラックス"}, {name: "チノパン"}])
 interiors_kitchen.children.create([{name: "食器"}, {name: "調理器具"}])
 interiors_bed.children.create([{name: "シングルベッド"}, {name: "マットレス"}])
 
-Item.create!([{name: "ラルフローレン ポロシャツ レディース",
-            price: "1500",
-            item_condition: "目立った傷や汚れなし",
-            delivery_fee: "送料込み(出品者負担)",
-            shipping_date: "2~3日で発送",
-            discription: "ラルフローレンのポロシャツです。",
-            shipping_rule: "未定",
-            prefecture_id: "1",
-            status: "出品中",
-            user_id: "1"
-            },
+Item.create!([{
+  name: "新品ワイドパンツ",
+  price: "2280",
+  item_condition: "目立った傷や汚れなし",
+  delivery_fee: "送料込み(出品者負担)",
+  shipping_date: "2~3日で発送",
+  discription: "お値下げ致しました。新品のワイドパンツカーキです。",
+  shipping_rule: "未定",
+  prefecture_id: "1",
+  status: "出品中",
+  user_id: "1"},
 
-            {name: "ラルフローレン ポロシャツ レディース",
-            price: "1500",
-            item_condition: "目立った傷や汚れなし",
-            delivery_fee: "送料込み(出品者負担)",
-            shipping_date: "2~3日で発送",
-            discription: "ラルフローレンのポロシャツです。",
-            shipping_rule: "未定",
-            prefecture_id: "1",
-            status: "出品中",
-            user_id: "1"
-            },
+  {name: "大塚愛 Tシャツ",
+  price: "1500",
+  item_condition: "新品、未使用",
+  delivery_fee: "送料込み(出品者負担)",
+  shipping_date: "1~2日で発送",
+  discription: "オーガニックＴシャツです。撮影の為に、袋から出しました",
+  shipping_rule: "未定",
+  prefecture_id: "12",
+  status: "出品中",
+  user_id: "1"
+  },
 
-            {name: "ラルフローレン ポロシャツ レディース",
-            price: "1500",
-            item_condition: "目立った傷や汚れなし",
-            delivery_fee: "送料込み(出品者負担)",
-            shipping_date: "2~3日で発送",
-            discription: "ラルフローレンのポロシャツです。",
-            shipping_rule: "未定",
-            prefecture_id: "1",
-            status: "出品中",
-            user_id: "1"
-            },
+  {name: "ラルフローレン ポロシャツ レディース",
+  price: "3000",
+  item_condition: "新品、未使用",
+  delivery_fee: "送料込み(出品者負担)",
+  shipping_date: "1~2日で発送",
+  discription: "セパレートタイプなので上下別々となっています！",
+  shipping_rule: "未定",
+  prefecture_id: "34",
+  status: "出品中",
+  user_id: "1"
+  },
 
-            {name: "ラルフローレン ポロシャツ レディース",
-            price: "1500",
-            item_condition: "目立った傷や汚れなし",
-            delivery_fee: "送料込み(出品者負担)",
-            shipping_date: "2~3日で発送",
-            discription: "ラルフローレンのポロシャツです。",
-            shipping_rule: "未定",
-            prefecture_id: "1",
-            status: "出品中",
-            user_id: "1"
-            }])
+  {name: "レーヨンアサ ブラウス",
+  price: "9244",
+  item_condition: "目立った傷や汚れなし",
+  delivery_fee: "送料込み(出品者負担)",
+  shipping_date: "2~3日で発送",
+  discription: "大人気で即完売だった商品です。
+  新品未使用タグ付き。
+  お探しだった方、ぜひ(*^^*)",
+  shipping_rule: "未定",
+  prefecture_id: "8",
+  status: "出品中",
+  user_id: "1"
+  }])
+
+
+item1 = Item.find(1)
+item1.images.attach(io: File.open('public/images/test_image1.jpg'), filename: 'test_image1.jpg')
+
+item1 = Item.find(2)
+item1.images.attach(io: File.open('public/images/test_image2.jpg'), filename: 'test_image1.jpg')
+
+item1 = Item.find(3)
+item1.images.attach(io: File.open('public/images/test_image3.jpg'), filename: 'test_image1.jpg')
+
+item1 = Item.find(4)
+item1.images.attach(io: File.open('public/images/test_image4.jpg'), filename: 'test_image1.jpg')
