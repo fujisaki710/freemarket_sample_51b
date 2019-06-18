97020f5428d005dc0abfa1b3bb00ea237cc53455
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2019-06-18 16:11:45 +0900

    deivise_scopeの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
-  
+  devise_scope :user do
+    get '/form', to: 'devise/registrations#form'
+  end
   root 'items#index'
 
   resources :items do
