b81ef3c4cf9a7b2d4adbee61aa23a1acb046ce3c
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-06-18 11:44:57 +0900

    fix:conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root 'items#index'
   resources :items do
-    collection do
-      get 'search', to: 'items#search'
+    member do
+      get 'purchase', to: 'items#purchase'
+      post 'pay', to: 'items#pay'
+      get 'done', to: 'items#done'
     end
   end
   resources :users do
