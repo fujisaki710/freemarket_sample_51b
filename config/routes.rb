be3704ad820cab0beb9e880e7305c5230cda667c
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-06-18 11:47:16 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root 'items#index'
+
   resources :items do
     member do
       get 'purchase', to: 'items#purchase'
